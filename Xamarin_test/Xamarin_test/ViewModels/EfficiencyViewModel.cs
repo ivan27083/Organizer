59c2f8eee10d02b19faefcc8971d99934f5a90d6
Kristina119 <lylena.br18@gmail.com>
2024-05-02 00:35:23 +0300

    efficiency page

--- Xamarin_test/Xamarin_test/ViewModels/EfficiencyViewModel.cs
+++ Xamarin_test/Xamarin_test/ViewModels/EfficiencyViewModel.cs
@@ -5,6 +5,8 @@ using System.Text;
 using SkiaSharp;
 using Microcharts;
 using Xamarin.Forms;
+using System.Numerics;
+using Xamarin.Essentials;
 
 
 namespace Xamarin_test.ViewModels
@@ -14,7 +16,7 @@ namespace Xamarin_test.ViewModels
         public EfficiencyViewModel()
         {
             Title = "Efficiency";
-            InitData();
+            InitData_barchart();
         }
 
         private BarChart barChart;
@@ -23,48 +25,95 @@ namespace Xamarin_test.ViewModels
             get => barChart;
             set => SetProperty(ref barChart, value);
         }
-
-        private void InitData()//ff0000  3498db
+        private void InitData_barchart()//ff0000  3498db
         {
-            float maxvalue = 1000;
+            int maxvalue = 1000;
+            float v1=300, v2=900, v3=700, v4=500;
+            int p1 = (int)Math.Ceiling(v1) * 100 / maxvalue,
+                p2 = (int)Math.Ceiling(v2) * 100 / maxvalue,
+                p3 = (int)Math.Ceiling(v3) * 100 / maxvalue,
+                p4 = (int)Math.Ceiling(v4) * 100 / maxvalue;
+
+            int r0=166, r=255, g0=4, g=192, b0=0, b=64;
+
+            string
+                c1 = "#" + Convert.ToString((int)Math.Ceiling(r0 + 0.89 * p1), 16) + Convert.ToString((int)Math.Ceiling(g0 + 1.88 * p1), 16) + Convert.ToString((int)Math.Ceiling(b0 + 0.64 * p1), 16),
+                c2 = "#" + Convert.ToString((int)Math.Ceiling(r0 + 0.89 * p2), 16) + Convert.ToString((int)Math.Ceiling(g0 + 1.88 * p2), 16) + Convert.ToString((int)Math.Ceiling(b0 + 0.64 * p1), 16),
+                c3 = "#" + Convert.ToString((int)Math.Ceiling(r0 + 0.89 * p3), 16) + Convert.ToString((int)Math.Ceiling(g0 + 1.88 * p3), 16) + Convert.ToString((int)Math.Ceiling(b0 + 0.64 * p1), 16),
+                c4 = "#" + Convert.ToString((int)Math.Ceiling(r0 + 0.89 * p4), 16) + Convert.ToString((int)Math.Ceiling(g0 + 1.88 * p4), 16) + Convert.ToString((int)Math.Ceiling(b0 + 0.64 * p1), 16);
+
+
             var backgroundColor = SKColor.Parse("#4AB4E4E7"); // Цвет заднего фона
             var blueColor = SKColor.Parse("#FF9040");
             var chartEntries = new List<ChartEntry>
             {
-                new ChartEntry(300)
+                new ChartEntry(v1)
                 {
                     Label = "01.05",
-                    //ValueLabel = "300",
-                    ValueLabel = (300*100/maxvalue).ToString()+" %",
-                    Color = blueColor
+                    ValueLabel = p1.ToString()+"%",
+                    Color = SKColor.Parse(c1)
                 },
-                new ChartEntry(500)
+                new ChartEntry(v2)
                 {
                     Label = "02.05",
-                    ValueLabel = (500*100/maxvalue).ToString()+" %",
-                    Color = blueColor
+                    ValueLabel = p2.ToString()+"%",
+                    Color = SKColor.Parse(c2)
                 },
-                new ChartEntry(700)
+                new ChartEntry(v3)
                 {
                     Label = "03.05",
-                    ValueLabel = (700*100/maxvalue).ToString()+" %",
-                    Color = blueColor
+                    ValueLabel = p3.ToString()+"%",
+                    Color = SKColor.Parse(c3)
                 },
-                new ChartEntry(400)
+                new ChartEntry(v4)
                 {
                     Label = "04.05",
-                    ValueLabel = (400*100/maxvalue).ToString()+" %",
-                    Color = blueColor
+                    ValueLabel = p4.ToString()+"%",
+                    Color = SKColor.Parse(c4)
+                },
+                 new ChartEntry(v4)
+                {
+                    Label = "04.05",
+                    ValueLabel = p4.ToString()+"%",
+                    Color = SKColor.Parse(c4)
+                },
+                  new ChartEntry(v3)
+                {
+                    Label = "04.05",
+                    ValueLabel = p3.ToString()+"%",
+                    Color = SKColor.Parse(c3)
+                },
+                   new ChartEntry(v4)
+                {
+                    Label = "04.05",
+                    ValueLabel = p4.ToString()+"%",
+                    Color = SKColor.Parse(c4)
+                },
+                    new ChartEntry(v2)
+                {
+                    Label = "04.05",
+                    ValueLabel = p2.ToString()+"%",
+                    Color = SKColor.Parse(c2)
+                },
+                     new ChartEntry(v1)
+                {
+                    Label = "04.05",
+                    ValueLabel = p1.ToString()+"%",
+                    Color = SKColor.Parse(c1)
                 },
             };
 
             BarChart = new BarChart { Entries = chartEntries,
                 BackgroundColor = backgroundColor,
-                LabelTextSize = 30f,
+                LabelTextSize = 35f,
                 MaxValue = maxvalue,
-                LabelOrientation = Orientation.Horizontal,
+                //LabelOrientation = Orientation.Horizontal,
                 ValueLabelOrientation = Orientation.Horizontal };
         }
+
+        public string Text1_notdone { get; set; } = "Количество невыполненных задач: " + "10";
+        public string Text2_done { get; set; } = "Количество выполненных задач: " + "40";
+        public string Text3_aims { get; set; } = "Ближайшие задачи: ";
     }
 }
 
